ff68fc3fbe207be22153df532fdec88e396a006a
arutyunyan <8david@inbox.ru>
2022-02-13 16:16:34 +0300

    Добавил ссылку на обратную связь
    Добавил ссылку на обратную связь

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -1,11 +1,12 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandHelp
 
+from data.config import FEEDBACK_LINK
 from loader import dp
 
 
 @dp.message_handler(CommandHelp())
 async def bot_help_command(message: types.Message):
     await message.answer(
-        "🤖 Что я могу?\n    • /start – регистрация с данными личного кабинета Мосполитеха.\n    • /schedule – расписание твоей группы на сегодня.\n    • /profile – профиль студента и приказы.\n    • /performance – успеваемость студента"
+        f"🤖 Что я могу?\n    • /start – регистрация с данными личного кабинета Мосполитеха;\n    • /schedule – расписание твоей группы на сегодня;\n    • /profile – профиль студента и приказы;\n    • /performance – успеваемость студента.\n\n🔧 <a href='{FEEDBACK_LINK}'>Обратная связь для багов.</a>"
     )
