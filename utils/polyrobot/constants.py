ec806f2f0c9330371607b25d883dd24af10c1a38
arutyunyan <8david@inbox.ru>
2022-04-10 16:11:33 +0300

    Настройка уведомдений о появлении нового расписания

--- utils/polyrobot/constants.py
+++ utils/polyrobot/constants.py
@@ -10,16 +10,20 @@ WEEKDAYS = {
 
 NOTIFICATION_SLUG_TO_BUTTON_TEXT = {
     "remind-in-minutes": "⏰ Напоминания о начале занятий",
+    "notify-about-new-schedule": "🔔 Уведомление о новом расписании",
 }
 
 NOTIFICATION_SLUG_TO_UPDATE_MESSAGE_TEXT = {
     "remind-in-minutes": "⏰ За сколько минут до начала занятия мне прислать уведомление?",
+    "notify-about-new-schedule": "🔔 Хочешь ли ты получать уведомления об изменении расписания?",
 }
 
 NOTIFICATION_SLUG_TO_UPDATE_BUTTON_VALUES = {
     "remind-in-minutes": [0, 5, 10, 15, 30],
+    "notify-about-new-schedule": [],
 }
 
 NOTIFICATION_SLUG_TO_SWITCHABLE = {
     "remind-in-minutes": True,
+    "notify-about-new-schedule": True,
 }
