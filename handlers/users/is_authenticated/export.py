04fae157f03bce5c95a84b430a861e8c5a888978
arutyunyan <8david@inbox.ru>
2022-06-12 16:09:04 +0300

    Переименовал функцию

--- handlers/users/is_authenticated/export.py
+++ handlers/users/is_authenticated/export.py
@@ -8,7 +8,7 @@ from utils.polyrobot.user import User
 
 
 @dp.message_handler(commands=["export"], is_authenticated=True, state="*")
-async def bot_profile_command(message: Message):
+async def bot_export_command(message: Message):
     user = User(message.from_user.id, message.from_user.full_name, message.from_user.username)
     calendar = await user.export_schedule()
     file = InputFile(BytesIO(calendar), f"Расписание от {datetime.now(tz=tz).date().isoformat()}.ics")
